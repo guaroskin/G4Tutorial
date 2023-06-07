4690e036f695c2ade5137f48b003a5bf18f60a4e
guaroskin <nguaring@unal.edu.co>
2023-06-06 22:11:20 -0500

    First basic Code

---
+++ build/CMakeFiles/G4example02.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/G4example02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/G4example02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
