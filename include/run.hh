409627ada1fb33c01eea99ee5d0ba98120aa15ef
guaroskin <nguaring@unal.edu.co>
2023-06-20 16:01:18 -0500

    Creating Macro Files

--- include/run.hh
+++ include/run.hh
@@ -2,8 +2,10 @@
 #define RUN_HH
 
 #include "G4UserRunAction.hh"
+#include "G4Run.hh"
 
-#include "G4AnalysisManager.hh"
+//#include "G4AnalysisManager.hh"
+#include "g4root.hh"
 
 class MyRunAction : public G4UserRunAction
 {
